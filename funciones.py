76fa62198902339c6e769c8baeb5241706a2621b
Maximiliano Sanchez <makzofx@gmail.com>
2022-08-12 01:13:19 -0300

    primeros metodos de volatilidad no funcionales aun

--- funciones.py
+++ funciones.py
@@ -1,4 +1,5 @@
 import math as ma
+import statistics
 
 #acá calculo la formula de la variación
 def variacion (actual,anterior):
@@ -11,8 +12,36 @@ def calculoVariacion(lista):
         lista_variacion.append(variacion(lista[i],lista[i-1]))  
     return lista_variacion;
     
+#Volatilidad de 6 periodos - volatilidad de x periodos
 
-#lista = [100,300,750,1000]
+def desviacionEstandar (lista):
+    return statistics.stdev(lista)
+
+#metodo para armar la sublista que quiero recorrer en cada iteración
+def armarSublista(lista,inicio,fin):
+    sublista = []
+    while(i < len(lista)):
+        list.append(lista[i:i+n])
+        #i += n
+    return list; 
+
+#metodo de volatilidad
+def volatilidadxPeriodos (lista, indice):
+    lista_volatilidad = []
+    for i in range (indice,len(lista),1):
+        lista_volatilidad.append(0)
+    return lista_volatilidad;
+
+
+
+
+
+
+
+lista = [100,300,750,1000,2000,3000,5000]
+
+print(volatilidadxPeriodos(lista,5))
+
 #resultado = [0,200,450,250]
 #print(calculoVariacion(lista))
 
